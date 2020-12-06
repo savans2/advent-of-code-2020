59a917e81b698e88ca6ce3590b27d4a12163e8cf
Sava <sava.paroski021@gmail.com>
2020-12-06 19:48:19 +0100

    Answer

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { instructions } from './data';
+
+let validPasswordCount = 0;
+instructions.forEach(instruction => {
+
+  const instructionAsArray = instruction.split(' ');
+  const minAndMax = instructionAsArray[0].split('-');
+
+  const min = parseInt(minAndMax[0])  // 1
+  const max = parseInt(minAndMax[1])  // 3
+  const char = instructionAsArray[1].split(':')[0]; // "h"
+  const passwordAsString = instructionAsArray[2]; // "abcdh"
+  const passwordAsArray = instructionAsArray[2].split(''); // ["a","b","c","d","h"]
+
+  let charRepeatCount = 0;
+  passwordAsArray.forEach(pwChar => {
+    if (char === pwChar) charRepeatCount++;
+  })
+
+  if (charRepeatCount >= min && charRepeatCount <= max) {
+    validPasswordCount++;
+  }
+
+
+});
+console.log(validPasswordCount);
