9febfd38855a0d5759dd28e4707251bba4f48178
Sava <sava.paroski021@gmail.com>
2020-12-06 21:59:29 +0100

    Answer

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import { convertArrayToObject } from "./utils";
 
 /*
-  Suppose your expense report contained the following:
-  [ 1721, 979, 366, 299, 675, 1456]
-  In this list, the two entries that sum to 2020 are 1721 and 299.
-  Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+  The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation.
+  They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+  Using the above example again, the three entries that sum to 2020 are 979, 366, and 675.
+  Multiplying them together produces the answer, 241861950.
+
+  In your expense report, what is the product of the three entries that sum to 2020?
 */
 const expenseReports = [
   1688, 1463, 1461, 1842, 1441, 1838, 1583, 1891, 1876, 1551, 1506, 2005, 1989, 1417, 1784,
@@ -23,18 +26,17 @@ const expenseReports = [
   1920, 1493, 1968, 1564, 1572
 ];
 
-const expenseReportsObj = convertArrayToObject(expenseReports, 2020);
 
-console.log(expenseReportsObj);
+/* 
+  TODO: Optimize❗❗❗❗❗
+    O(n^3)💀🔫
+*/
 
-
-
-Object.keys(expenseReportsObj).forEach(key => {
-
-  if (expenseReportsObj[expenseReportsObj[key]] !== undefined)
-    console.log(key, expenseReportsObj[key], parseInt(key) * expenseReportsObj[key])
-
-  //TODO: Breakout of this loop
-
+expenseReports.forEach(element1 => {
+  expenseReports.forEach(element2 => {
+    expenseReports.forEach(element3 => {
+      if (element1 + element2 + element3 === 2020)
+        console.log(element1 * element2 * element3);
+    });
+  });
 })
-
