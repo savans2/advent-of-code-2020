b22e083b9a94d39741c35bf362d9b5853eaa20e9
Sava <sava.paroski021@gmail.com>
2020-12-06 20:16:44 +0100

    Answer

--- index.ts
+++ index.ts
@@ -4,22 +4,17 @@ let validPasswordCount = 0;
 instructions.forEach(instruction => {
 
   const instructionAsArray = instruction.split(' ');
-  const minAndMax = instructionAsArray[0].split('-');
+  const positions = instructionAsArray[0].split('-');
 
-  const min = parseInt(minAndMax[0])  // 1
-  const max = parseInt(minAndMax[1])  // 3
+  const positionOne = parseInt(positions[0])  // 1
+  const positionTwo = parseInt(positions[1])  // 3
   const char = instructionAsArray[1].split(':')[0]; // "h"
   const passwordAsArray = instructionAsArray[2].split(''); // ["a","b","c","d","h"]
 
-  let charRepeatCount = 0;
-  passwordAsArray.forEach(pwChar => {
-    if (char === pwChar) charRepeatCount++;
-  })
+  const isAtFirstPosition = char === passwordAsArray[positionOne - 1] && !(char == passwordAsArray[positionTwo - 1]);
+  const isAtSecondPosition = char == passwordAsArray[positionTwo - 1] && !(char === passwordAsArray[positionOne - 1]);
 
-  if (charRepeatCount >= min && charRepeatCount <= max) {
-    validPasswordCount++;
-  }
-
+  if (isAtFirstPosition || isAtSecondPosition) validPasswordCount++;
 
 });
 console.log(validPasswordCount);
