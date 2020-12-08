8df241c056cb13f115770fb0a4179a14876d7aca
Sava <sava.paroski021@gmail.com>
2020-12-08 22:08:34 +0100

    Answer

--- index.ts
+++ index.ts
@@ -19,20 +19,37 @@ inputAsArray.forEach((field, i) => {
   }
 });
 
+const treeCountArray = [];
+for (let index = 0; index < 5; index++) {
+  let position_increment = 0;
+  let row_increment = 1;
+  if (index === 0) position_increment = 1;
+  if (index === 1) position_increment = 3;
+  if (index === 2) position_increment = 5;
+  if (index === 3) position_increment = 7;
+  if (index === 4) {
+    position_increment = 1;
+    row_increment = 2;
+  }
 
-let treeCount = 0;
-let position = 0;
-for (let i = 1; i < map.length; i++) {
+  let treeCount = 0;
+  let position = 0;
+  for (let i = 0; i < map.length; i += row_increment) {
+    if (map[i + row_increment] === undefined) break;
 
-  position = (position + 3) % 31
-  const field = map[i][position];
-  if (field === '#') treeCount++;
+    position = (position + position_increment) % 31
+    const field = map[i + row_increment][position];
+    if (field === '#') treeCount++;
+  }
 
-  map[i][position] = 'X';
-  console.log(map[i].join(''), map[i].length);
+  treeCountArray.push(treeCount);
+
 }
+console.log(treeCountArray);
 
-console.log(treeCount);
+console.log(
+  treeCountArray.reduce((accumulator, currentValue) => accumulator * currentValue)
+);
 
 
 
